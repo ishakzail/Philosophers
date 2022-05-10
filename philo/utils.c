f79f83ea546948634eaabeaccd5b18ed0c073c5b
ishakzail <ishak.zail.iz@gmail.com>
2022-05-10 17:46:59 +0100

    add util functions

---
+++ philo/utils.c
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: izail <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/10 14:55:56 by izail             #+#    #+#             */
+/*   Updated: 2022/05/10 14:55:57 by izail            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	ft_atoi(const char *str)
+{
+	int			signe;
+	long		nbr;
+
+	signe = 1;
+	nbr = 0;
+	while (*str == ' ' || *str == '\n' || *str == '\t' || *str == '\r'
+		|| *str == '\f' || *str == '\v')
+		str++;
+	if (*str == '+')
+		str++;
+	else if (*str == '-')
+	{
+		signe = -1;
+		str++;
+	}
+	while (*str && (*str >= '0' && *str <= '9'))
+	{
+		nbr = (nbr * 10) + (*str - '0');
+		str++;
+	}
+	return (nbr * signe);
+}
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	char			*p;
+	unsigned int	total;
+	unsigned int	i;
+
+	total = count * size;
+	p = (char *)malloc(total * sizeof(char));
+	if (!p)
+		return (0);
+	i = 0;
+	while (total--)
+	{
+		p[i] = 0;
+		i++;
+	}
+	return ((void *)p);
+}
+
+long long	ft_get_time(void)
+{
+	t_time	current_time;
+
+	gettimeofday(&current_time, NULL);
+	return (current_time.tv_sec * 1000 + current_time.tv_usec / 1000);
+}
+
+long long	ft_current_time(t_philo *philo)
+{
+	long long	time;
+
+	time = ft_get_time() - philo->info->creation_time;
+	return (time);
+}
+
+int	ft_puterr(char *err)
+{
+	write(STDERR_FILENO, err, ft_strlen(err));
+	return (FAILURE);
+}
+
+int	ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
