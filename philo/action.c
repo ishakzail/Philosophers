c569d63f2b5460102b1b624718c06c9adb4b97a5
ishakzail <ishak.zail.iz@gmail.com>
2022-05-18 19:36:42 +0100

    updated

--- philo/action.c
+++ philo/action.c
@@ -22,44 +22,23 @@ void    put_fork(t_philo *philo, int f1 , int f2)
 {
     pthread_mutex_unlock(&philo->info->forks[f1]);
     pthread_mutex_unlock(&philo->info->forks[f2]);
-    ft_print_msg(philo, "put forks\n");
 }
 
 void    eat(t_philo *philo)
 {
     ft_print_msg(philo, "is eating\n");
-    philo->ate++;
-    if (philo->ate == philo->info->number_of_meals)
+	usleep(philo->info->time_to_eat * 1000);
+	philo->last_meal = ft_get_time();
+	philo->num_ate++;
+	if (philo->num_ate == philo->info->number_of_meals)
 		philo->info->all_ate++;
 }
 
-void    ft_sleep(t_philo *philo)
+void	sleep_then_think(t_philo *philo)
 {
-    ft_print_msg(philo, "is sleeping\n");
-}
-
-void    think(t_philo *philo)
-{
-    ft_print_msg(philo, "is thinking\n");
-}
-
-void    *ft_check_if_is_dead(void *arg)
-{
-    t_philo *philo;
-
-    philo = (void *)arg;
-    int i = 0;
-	while (!philo->info->flag)
-	{
-		if ((philo->last_meal + philo->info->time_to_die) < ft_get_time())
-		{
-			ft_print_msg(philo, "died");
-			philo->should_die = 1;
-			philo->info->flag = 1;
-		}
-		usleep(100);
-	}
-	return (NULL);
+	ft_print_msg(philo, "is sleeping\n");
+	usleep(philo->info->time_to_sleep * 1000);
+	ft_print_msg(philo, "is thinking\n");
 }
 
 void    *action(void *arg)
@@ -69,23 +48,24 @@ void    *action(void *arg)
     int r_fork;
 
     philo = (t_philo *)arg;
-    int i = 0;
-    while (!philo->should_die)
+    while (!philo->should_die && !philo->info->flag)
     {
         r_fork = philo->id;
+        // if (philo->id == philo->info->number_of_philosophers)
+        //     l_fork = 0;
+        // else
         l_fork = (philo->id + 1) % philo->info->number_of_philosophers;
         take_fork(philo, r_fork);
         if (philo->info->number_of_philosophers == 1)
         {
-            usleep(100000);
+            usleep(80000);
+            // ft_print_msg(philo, "died\n");
             break;
         }
         take_fork(philo, l_fork);
         eat(philo);
         put_fork(philo, r_fork, l_fork);
-        ft_sleep(philo);
-        think(philo);
-        // philo->info->number_of_meals--;
+        sleep_then_think(philo);
     }
     return (NULL);
 }
@@ -93,15 +73,26 @@ void    *action(void *arg)
 void    create_philo(t_info *info)
 {
     int i = 0;
+    pthread_t death;
+
+    
     info->creation_time = ft_get_time();
-    while (i < info->number_of_philosophers)
+    while (i < (info->number_of_philosophers))
     {
         info->philo[i].id = i;
         info->philo[i].info = info;
         info->philo[i].last_meal = info->creation_time; 
         info->philo->should_die = 0;
+        info->philo[i].num_ate = 0;
         pthread_create(&info->philo[i].thread, NULL, &action, &info->philo[i]);
+        pthread_create(&death, NULL, &check_death, &info->philo[i]);
+        pthread_detach(death);
         i++;
-        usleep(100);
+        usleep(1000);
+    }
+    if (info->number_of_meals >= 0)
+    {
+        pthread_create(&death, NULL, check_all_ate, info);
+		pthread_detach(death);
     }
 }
